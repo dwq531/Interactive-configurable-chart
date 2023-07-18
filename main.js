bd9beaa694e2fd4efa76f542327fe445e5cf3662
duwanqing <duwanqingdwq@163.com>
2023-07-18 22:21:13 +0800

    优化了界面样式，但还要改。折叠菜单初步实现，缺少动画，没封装成组件。

---
+++ main.js
@@ -0,0 +1,20 @@
+window.onload = function ()
+{
+    var editorBoxs = document.getElementsByClassName('editor-box-title');
+    console.log(editorBoxs);
+    for(let box of editorBoxs)
+    {
+        box.addEventListener("click",editor_box_clicked);
+        console.log(box);
+    }
+        
+};
+function editor_box_clicked()
+{
+    var parent = this.parentElement;
+    var child=parent.children[1];
+    if(child.className==='editor-box-hidden')
+        child.className='editor-box-content';
+    else
+        child.className='editor-box-hidden';
+};
