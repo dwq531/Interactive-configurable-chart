76a3aa65ed3c22092d0e656869af92a3b44a4777
duwanqing <duwanqingdwq@163.com>
2023-07-20 20:42:27 +0800

    尝试避免有可能出现的合并冲突

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 window.onload = () => {
     let buts = document.getElementsByClassName("sidenav-button");
-    for(let but of buts)
-    {
-        but.addEventListener("click",sidenavbut_clicked);
+    for (let but of buts) {
+        but.addEventListener("click", sidenavbut_clicked);
     }
-    buts=document.getElementsByClassName("sidenav-button-clicked");
-    for(let but of buts)
-    {
-        but.addEventListener("click",sidenavbut_clicked);
+    buts = document.getElementsByClassName("sidenav-button-clicked");
+    for (let but of buts) {
+        but.addEventListener("click", sidenavbut_clicked);
     }
+    // dwq
+
+    // dwq
+
+    // cz
+
+    // cz
+
+    // gff
+
+    // gff
 }
 // 侧边栏导航按钮点击事件
-function sidenavbut_clicked()
-{
+function sidenavbut_clicked() {
     let buts = document.getElementsByClassName("sidenav-button-clicked");
-    for(let but of buts)
-    {
-        but.className="sidenav-button";
+    for (let but of buts) {
+        but.className = "sidenav-button";
     }
-    this.className="sidenav-button-clicked";
+    this.className = "sidenav-button-clicked";
 }
+// dwq
+
+// dwq
+
+// cz
+
+// cz
+
+// gff
+
+// gff
