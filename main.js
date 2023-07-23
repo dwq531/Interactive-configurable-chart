539e1b94b661bc1067373f47a73ebd2a2e3a3319
duwanqing <duwanqingdwq@163.com>
2023-07-23 13:41:29 +0800

    开写

--- main.js
+++ main.js
@@ -13,11 +13,19 @@ window.onload = () => {
     canvas.height = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--chartheight').trim());
     canvas.width = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--chartwidth').trim());
     var ctx = canvas.getContext('2d');
+    var datamin=0;
+    var datamax;
     
     // dwq
     var data = getData();// 存输入数据的二维数组
     var padding=100;
-    drawAxis(canvas,ctx,padding,canvas.height-padding,canvas.width-padding,padding,data.length,data);
+    // x0,y1
+    //   |
+    // x0,y0------x1,y0
+    var x0=padding,y0=canvas.height-padding,x1=canvas.width-padding,y1=padding;
+    var delta=(x1-x0)*1.0/data.length;// 点之间的距离
+    var first=x0+delta/2; // 第一个点的位置
+    drawAxis(canvas,ctx,x0,y0,x1,y1,data.length,data,delta,first);
     
     // dwq
 
@@ -42,10 +50,8 @@ function getData()
 {
     return [[2019,2],[2020,3],[2021,5],[2022,4]];
 }
-// x0,y1
-//   |
-// x0,y0------x1,y0
-function drawAxis(canvas,ctx,x0,y0,x1,y1,numOfData,data)
+
+function drawAxis(canvas,ctx,x0,y0,x1,y1,numOfData,data,delta,first)
 {
     ctx.strokeStyle = "black";
     ctx.beginPath();
@@ -56,14 +62,13 @@ function drawAxis(canvas,ctx,x0,y0,x1,y1,numOfData,data)
     ctx.moveTo(x0+0.5,y0+0.5);
     ctx.lineTo(x1+0.5,y0+0.5);
     ctx.stroke();
-    let delta=(x1-x0)*1.0/numOfData;
     ctx.textAlign="center";
     for(let i=1;i<=numOfData;i++)
     {
         ctx.moveTo(x0+i*delta+0.5,y0+0.5);
         ctx.lineTo(x0+i*delta+0.5,y0+8.5);
         ctx.stroke();
-        ctx.fillText(data[i-1][0],x0+(i-0.5)*delta+0.5,y0+20.5);
+        ctx.fillText(data[i-1][0],first+(i-1)*delta+0.5,y0+20.5);
     }
 }
 // dwq
