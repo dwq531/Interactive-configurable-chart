0c0e42b4668924fcb611c14e1595683781f9754b
duwanqing <duwanqingdwq@163.com>
2023-07-23 21:55:39 +0800

    封装参数

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var params =new Object();
 window.onload = () => {
     // 侧边栏导航按钮添加事件
     let buts = document.getElementsByClassName("sidenav-button");
@@ -8,34 +9,42 @@ window.onload = () => {
     for (let but of buts) {
         but.addEventListener("click", sidenavbut_clicked);
     }
+    params.constructor();
+    params.paint();
+}
+params.constructor = function()
+{
     // 用canvas绘制图表
-    var canvas = document.getElementById("canvas");
+    this.canvas = document.getElementById("canvas");
     canvas.height = 2*parseInt(getComputedStyle(document.documentElement).getPropertyValue('--chartheight').trim());
     canvas.width = 2*parseInt(getComputedStyle(document.documentElement).getPropertyValue('--chartwidth').trim());
     canvas.style.width = (canvas.width/2)+"px";
     canvas.style.height = (canvas.height/2)+"px";
-    var ctx = canvas.getContext('2d');
+    this.ctx = canvas.getContext('2d');
     
     
     // dwq
-    var data = getData();// 存输入数据的二维数组
-    var datamin = Math.max(data);
-    var datamax = Math.min(data);
-    var padding=150;
+    this.data = this.getData();// 存输入数据的二维数组
+    this.datamin = Math.max(this.data);
+    this.datamax = Math.min(this.data);
+    this.padding=150;
     // x0,y1
     //   |
     // x0,y0------x1,y0
-    var x0=padding,y0=canvas.height-padding,x1=canvas.width-padding,y1=padding;
-    var delta=(x1-x0)*1.0/data.length;// 点之间的距离
-    var first=x0+delta/2; // 第一个点的位置
-    var dh = (y0-y1-20)/6; // y轴刻度间隔像素
-    var dnum = 1;// y轴单位刻度数值的增长
-    var axisColor = "blue";
-    drawAxis(canvas,ctx,x0,y0,x1,y1,axisColor);
-    darwScaleLine(x0,y0,ctx,delta,data,first,dh,dnum);
+    this.x0=this.padding,this.y0=this.canvas.height-this.padding,this.x1=this.canvas.width-this.padding,this.y1=this.padding;
+    this.delta=(this.x1-this.x0)*1.0/this.data.length;// 点之间的距离
+    this.first=this.x0+this.delta/2; // 第一个点的位置
+    this.dh = (this.y0-this.y1-20)/6; // y轴刻度间隔像素
+    this.dnum = 1;// y轴单位刻度数值的增长
+    this.axisColor = "blue";
+}
+params.paint = function() 
+{
+    this.drawAxis();
+    this.darwScaleLine();
     
     // dwq
-
+    /*
     // cz
     drawHistogram(canvas,ctx,x0,y0,delta,data,dh,dnum);
     // cz
@@ -45,6 +54,7 @@ window.onload = () => {
     var sizeOfPoint = 10;
     drawLineChart(canvas,ctx,x0,y0,x1,y1,data.length,data,delta,first,shapeOfPoint,sizeOfPoint);
     // gff
+    */
 }
 // 侧边栏导航按钮点击事件
 function sidenavbut_clicked() {
@@ -55,7 +65,7 @@ function sidenavbut_clicked() {
     this.className = "sidenav-button-clicked";
 }
 // dwq
-function getData()
+params.getData = function()
 {
     let table = document.getElementById("dataInputTableBody");
     let data = new Array(0);
@@ -70,9 +80,10 @@ function getData()
     return data;
 }
 
-function drawAxis(canvas,ctx,x0,y0,x1,y1,color)
+params.drawAxis = function()
 {
-    ctx.strokeStyle = color;
+    let ctx=this.ctx,x0=this.x0,y0=this.y0,y1=this.y1,x1=this.x1;
+    ctx.strokeStyle = this.axisColor;
     ctx.beginPath();
     ctx.lineWidth=2;    
     ctx.moveTo(x0,y0);
@@ -103,26 +114,27 @@ function drawAxis(canvas,ctx,x0,y0,x1,y1,color)
     ctx.fillText("（万吨）",x0-90,y1+100);
     ctx.fillText("年份",x1+50,y0+50);
 }
-function darwScaleLine(x0,y0,ctx,delta,data,first,dh,dnum,color)
+params.darwScaleLine = function()
 {
-    ctx.strokeStyle = color;
+    let ctx=this.ctx,x0=this.x0,y0=this.y0,y1=this.y1,x1=this.x1,delta=this.delta;
+    ctx.strokeStyle = this.axisColor;
     ctx.textAlign="center";
     ctx.beginPath();
     ctx.lineWidth=2;
     ctx.font = "30px Microsoft YaHei";
-    for(let i=1;i<=data.length;i++)
+    for(let i=1;i<=this.data.length;i++)
     {
         ctx.moveTo(x0+i*delta,y0);
         ctx.lineTo(x0+i*delta,y0+15);
         ctx.stroke();
-        ctx.fillText(data[i-1][0],first+(i-1)*delta,y0+40);
+        ctx.fillText(this.data[i-1][0],this.first+(i-1)*delta,y0+40);
     }
     for(let i=1;i<=6;i++)
     {
-        ctx.moveTo(x0,y0-i*dh);
-        ctx.lineTo(x0-10,y0-i*dh);
+        ctx.moveTo(x0,y0-i*this.dh);
+        ctx.lineTo(x0-10,y0-i*this.dh);
         ctx.stroke();
-        ctx.fillText(i*dnum,x0-30,y0-i*dh+15);
+        ctx.fillText(i*this.dnum,x0-30,y0-i*this.dh+15);
     }
 }
 // dwq
